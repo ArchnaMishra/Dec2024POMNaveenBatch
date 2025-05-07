7495a83e1dee9256b82f1607aa6d542f1aba85e1
Archna Mishra <archna.mishra@outlook.com>
2025-05-07 06:30:11 +0530

    Changes done in fine as per review comment

--- src/main/java/com/qa/opencart/pages/GITTestClass.java
+++ src/main/java/com/qa/opencart/pages/GITTestClass.java
@@ -3,10 +3,12 @@ package com.qa.opencart.pages;
 public class GITTestClass {
 
 	int i=10;
+	int b=20;
 	
 	public void cart()
 	{
-		System.out.println("test method");
+		System.out.println("test method"); 
+		System.out.println("Updated method");
 	}
 			
 }
